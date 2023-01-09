4cd3fb2f3dac05c1bca80d205a35b871db0f691b
wbmcglaughlin <wmcg0001@student.monash.edu>
2023-01-09 12:54:21 +1100

    refactor: moved downloader to another file

---
+++ downloader.py
@@ -0,0 +1,18 @@
+import os
+
+from pytube import YouTube
+
+
+def download_set(name):
+    yt = YouTube(name)
+
+    # Check if path already exists
+    for path in os.listdir('./Videos'):
+        if name.split(sep='=')[-1] in path:
+            return
+
+    ys = yt.streams.filter(only_audio=False, only_video=False, progressive=True).get_lowest_resolution()
+
+    print("Downloading...")
+
+    ys.download(output_path='./Videos', filename=f'{name.split(sep="=")[-1]}.mp4')
--- main.py
+++ main.py
@@ -2,21 +2,7 @@ import os
 
 from pytube import YouTube
 
-
-def download_set(name):
-    yt = YouTube(name)
-
-    # Check if path already exists
-    for path in os.listdir('./Videos'):
-        if name.split(sep='=')[-1] in path:
-            return
-
-    ys = yt.streams.filter(only_audio=False, only_video=False, progressive=True).get_lowest_resolution()
-
-    print("Downloading...")
-
-    ys.download(output_path='./Videos', filename=f'{name.split(sep="=")[-1]}.mp4')
-
+from downloader import download_set
 
 if __name__ == '__main__':
     download_set('https://www.youtube.com/watch?v=VxV9ODD6IBs')
