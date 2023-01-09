53048a1ac3ce98ebbd5a97238f46663715c639a4
wbmcglaughlin <wmcg0001@student.monash.edu>
2023-01-09 15:27:19 +1100

    fix: seconds are accurately ripped

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import mediapipe as mp
 import numpy as np
 from matplotlib import pyplot as plt
-
+from pytube import YouTube
 from landmark_analysis import get_specific_landmark
 
 
 def main():
-    landmark_type = mp.solutions.pose.PoseLandmark.NOSE
-    specific_landmark = get_specific_landmark('WvyvwlowHWM', 'mp4', landmark_type)
+    yt_link_str = 'https://www.youtube.com/watch?v='
+    name = 'https://www.youtube.com/watch?v=WvyvwlowHWM'
+    result_hash = name.split('=')[-1]
+    yt = YouTube(name)
 
-    # time_per_frame = 20 / 30
+    landmark_type = mp.solutions.pose.PoseLandmark.NOSE
+    specific_landmark = get_specific_landmark(result_hash, 'mp4', landmark_type)
 
     x_width = 0.878 + 2 * 0.329
     y_width = x_width * 480 / 840
@@ -19,12 +22,13 @@ def main():
 
     print(f'Total Distance: {sum(distance_xy) / 1000:.3f} km')
 
-    # plt.xlim([0, 1])
-    # plt.ylim([0, 1])
-    # plt.plot(specific_landmark[:, 0], specific_landmark[:, 1])
+    plt.plot(specific_landmark[:, 0], specific_landmark[:, 1])
 
-    plt.plot(np.cumsum(distance_xy), label='WvyvwlowHWM')
+    time = np.linspace(0, yt.length, len(distance_xy))
 
+    plt.plot(time, np.cumsum(distance_xy), label=result_hash)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Cumulative Distance (m)')
     plt.legend()
     plt.show()
 
