8ffe30c9afb9539499a9ba81cad17fca5e7e7548
wbmcglaughlin <wmcg0001@student.monash.edu>
2023-01-09 14:51:09 +1100

    feature: specific landmark returned as np array

--- landmark_analysis.py
+++ landmark_analysis.py
@@ -4,13 +4,33 @@ import time
 
 import mediapipe as mp
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+landmark_legend = ['x', 'y', 'z', 'visibility']
+
+plt.style.use('seaborn-v0_8')
 
 
-def get_landmarks(path, type, calculate_again=False, save_video=False):
+def get_specific_landmark(path, path_suffix, pose_landmark: mp.solutions.pose.PoseLandmark):
+    ld = get_landmarks(path, path_suffix)
+    specific_landmark = np.array([landmark[pose_landmark] for landmark in ld])
+
+    return specific_landmark
+
+    # plt.xlim([0, 1])
+    # plt.ylim([0, 1])
+    # plt.plot(specific_landmark[:, 0], specific_landmark[:, 1])
+    # plt.legend(pose_landmark.name)
+    # plt.show()
+
+
+def get_landmarks(path, path_suffix, calculate_again=False, save_video=False):
     """
     Analyse videos landmarks.
-
-    :param type:
+    
+    :param save_video:
+    :param path_suffix:
     :param calculate_again:
     :param path: path to video
     :return:
@@ -28,7 +48,7 @@ def get_landmarks(path, type, calculate_again=False, save_video=False):
     pose = mp_pose.Pose()
     mp_draw = mp.solutions.drawing_utils
 
-    cap = cv2.VideoCapture(f'{path}.{type}')
+    cap = cv2.VideoCapture(f'Videos/{path}.{path_suffix}')
 
     if save_video:
         result = cv2.VideoWriter('swing.avi',
@@ -44,8 +64,6 @@ def get_landmarks(path, type, calculate_again=False, save_video=False):
         fps = 1 / (time.perf_counter() - last_frame)
         last_frame = time.perf_counter()
 
-        frame += 1
-
         print(f'\r{frame / total_frames * 100:.2f} % fps: {fps:.2f}', end='')
 
         success, img = cap.read()
@@ -56,6 +74,8 @@ def get_landmarks(path, type, calculate_again=False, save_video=False):
 
             if results.pose_landmarks:
                 mp_draw.draw_landmarks(img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+            else:
+                break
 
             landmark = []
             for idx, lm in enumerate(results.pose_landmarks.landmark):
@@ -69,6 +89,9 @@ def get_landmarks(path, type, calculate_again=False, save_video=False):
             # img = cv2.resize(img, (1080, 1920), cv2.INTER_AREA)
             if save_video:
                 result.write(img)
+
+            frame += 20
+            cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
 
             # cv2.imshow("Image", img)
             #
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from downloader import download_set
-from landmark_analysis import get_landmarks
+import mediapipe as mp
+
+from landmark_analysis import get_specific_landmark
+
+
+def main():
+    get_specific_landmark('qbcBZS6sMxc', 'mp4', mp.solutions.pose.PoseLandmark.NOSE)
+
 
 if __name__ == '__main__':
-    download_set('https://www.youtube.com/watch?v=VxV9ODD6IBs')
-    download_set('https://www.youtube.com/watch?v=qbcBZS6sMxc')
-    download_set('https://www.youtube.com/watch?v=WvyvwlowHWM')
-
-    get_landmarks('Videos/WvyvwlowHWM', 'mp4')
+    main()
