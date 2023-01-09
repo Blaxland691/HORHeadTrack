3bb92ce2115a95b7ec210977b6321d2611d6389d
wbmcglaughlin <wmcg0001@student.monash.edu>
2023-01-10 08:03:25 +1100

    feature: added analysis notebook

--- Modules/set.py
+++ Modules/set.py
@@ -1,4 +1,15 @@
+from pytube import YouTube
+
+from Modules.landmark_analysis import get_landmarks
+
+
 class HORSet:
     def __init__(self, link):
         self.link = link
         self.key = link.split('=')[-1]
+
+        # YouTube elements
+        self.yt = YouTube(link)
+        self.title = self.yt.title
+
+        self.landmarks = get_landmarks(self.key, 'mp4', frame_iter=20)
